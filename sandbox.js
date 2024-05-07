e97233a273247ebce9db32966f52cc32e2e0be1b
shyam <rajshyam151@gmail.com>
2024-05-07 11:32:51 +0530

    first commit

---
+++ sandbox.js
@@ -0,0 +1,28 @@
+var character = 'luigi';
+var age = 30;
+var isblack = false;
+character = 'luigi';
+age = 40;
+isblack = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
+// typeScript arrays 
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+names.push(3);
+var numbers = [10, 20, 30, 40];
+numbers.push('hd');
+var mixed = ['ken', 4, 'chun', 8];
+mixed.push('right');
+mixed.push(10);
+// objects 
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'he';
+ninja.skills = 'urn';
---
+++ sandbox.ts
@@ -0,0 +1,41 @@
+const character='luigi'
+ let age=30
+ let isblack=false
+
+  character ='luigi'
+    age=40
+
+      isblack=true
+
+      const circ=(diameter:number)=>{
+     return  diameter*Math.PI
+      }
+      console.log(circ(7.5))
+
+
+
+      // typeScript arrays 
+
+      let names=['luigi','mario','yoshi']
+      names.push('toad')
+      names.push(3)
+
+      let numbers=[10,20,30,40]
+      numbers.push('hd')
+
+
+      let mixed=['ken',4,'chun',8]
+      mixed.push('right')
+      mixed.push(10)
+
+
+      // objects 
+
+      let ninja= {
+        name: 'mario',
+        belt:'black',
+        age: 30
+      }
+      ninja.age=40
+      ninja.name='he'
+        ninja.skills=  'urn'
